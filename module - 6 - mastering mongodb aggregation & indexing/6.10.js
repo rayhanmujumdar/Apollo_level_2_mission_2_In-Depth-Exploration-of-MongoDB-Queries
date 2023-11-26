b968c3d44b57ec8ed5c28c2db54c983a969804b2
Rayhan Mojumdar <91730785+rayhanmujumdar@users.noreply.github.com>
2023-11-26 13:19:58 +0600

    Revert "Mongodb aggregation docs added"

--- module - 6 - mastering mongodb aggregation & indexing/6.10.js
+++
@@ -1,6 +0,0 @@
-// better performance to using indexing
-// db.massiveData.find({gender: 'male', age: 21}) // query time is 10ms
-// db.massiveData.createIndex({gender: -1, age: 1}) // create a index with gender or age field
-
-// db.massiveData.createIndex({about: 'text'})
-db.massiveData.find({$text: { $search: "dolor" }})
--- module - 6 - mastering mongodb aggregation & indexing/6.9.js
+++
@@ -1,7 +0,0 @@
-// This query search using COLLSCAN. this query searching algorithm is very slow for large scale data.
-// db.massiveData.find({email: 'boylemack@comveyor.com'}).explain('executionStats') // now query time is 8ms
-// db.massiveData.createIndex({"eamil": 1}) // create a indexes with email
-db.massiveData.find({email: 'boylemack@comveyor.com'}).explain() // now queye time is 0ms because now email is indexed
-
-// you can delete your indexes field
-// db.massiveData.dropIndex({email: 1})
